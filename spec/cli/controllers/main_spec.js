76125405347945fbda11efe3d39f20f700fe85d4
Julio Makdisse Saito <saitodisse@gmail.com>
2016-02-12 22:02:59 -0200

    [Spec] Fix specs

--- spec/cli/controllers/main_spec.js
+++ spec/cli/controllers/main_spec.js
@@ -10,22 +10,22 @@ describe('controller main:', () => {
   const getDocoptResult = (argv) => cli._cli.docopt({ exit: false, argv });
 
   it('should track one word', () => {
-    const docoptResult = getDocoptResult(['banana']);
+    const docoptResult = getDocoptResult(['--save', '/tmp', 'banana']);
     h.expect(docoptResult['<track>']).to.deep.equal(['banana']);
   });
 
   it('should track two words', () => {
-    const docoptResult = getDocoptResult(['banana', 'pizza']);
+    const docoptResult = getDocoptResult(['--save', '/tmp', 'banana', 'pizza']);
     h.expect(docoptResult['<track>']).to.deep.equal(['banana', 'pizza']);
   });
 
   it('should track one "two words"', () => {
-    const docoptResult = getDocoptResult(['banana pizza']);
+    const docoptResult = getDocoptResult(['--save', '/tmp', 'banana pizza']);
     h.expect(docoptResult['<track>']).to.deep.equal(['banana pizza']);
   });
 
   it('should `--save /tmp` be understood', () => {
-    const docoptResult = getDocoptResult(['banana', '--save', '/tmp']);
+    const docoptResult = getDocoptResult(['--save', '/tmp', 'banana']);
     h.expect(docoptResult['<track>']).to.deep.equal(['banana']);
     h.expect(docoptResult['--save']).to.deep.equal('/tmp');
   });
